48124aa3e5ed813e205d5099cb8fa4531736ab3f
ILSHAT <askils@mail.com>
2021-03-24 18:31:44 +0300

    v0

---
+++ Home_work_10_2/src/BoysList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class BoysList {
+    static final ArrayList<Boy> BOYS = new ArrayList<Boy>() {{
+        add(new Boy("Николай", 68));
+        add(new Boy("Пётр", 53));
+        add(new Boy("Василий", 25));
+        add(new Boy("Михаил", 19));
+        add(new Boy("Алексей", 6));
+        add(new Boy("Николай", 86));
+        add(new Boy("Пётр", 35));
+        add(new Boy("Михаил", 111));
+        add(new Boy("Алексей", 22));
+        add(new Boy("Михаил", 1));
+        add(new Boy("Яков", 30));
+    }};
+}
---
+++ Home_work_10_2/src/Main.java
@@ -0,0 +1,26 @@
+
+import java.util.stream.Collectors;
+
+
+public class Main {
+    private static final int AGE = 17;
+    private static final int LIM = 4;
+
+    public static void main(final String[] argc) {
+
+        BoysList.BOYS.stream().
+                filter(boy -> boy.getAge() > AGE).
+                map(Boy::getName).
+                distinct().sorted().
+                limit(LIM).
+                collect(Collectors.toMap((x -> x),
+                        (x -> BoysList.BOYS.stream().
+                                filter(boy -> boy.getName().
+                                        equals(x)).
+                                count() - 1))).
+                forEach((boy, rep) -> {
+                    System.out.println(boy + " = " + rep);
+                });
+    }
+
+}
